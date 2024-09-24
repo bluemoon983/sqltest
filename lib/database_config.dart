239dc37448d8fcbc9172a6185a2e68aef88a25dd
bluemoon983 <msdrobot@naver.com>
2024-09-24 10:16:23 +0900

    main

--- lib/database_config.dart
+++ lib/database_config.dart
@@ -4,6 +4,7 @@ import 'package:sqltest/word.dart';
 
 class DatabaseService {
   static final DatabaseService _database = DatabaseService._internal();
+
   late Future<Database> database;
 
   factory DatabaseService() => _database;
@@ -18,66 +19,71 @@ class DatabaseService {
         join(await getDatabasesPath(), 'word_database.db'),
         onCreate: (db, version) {
           return db.execute(
-            'CREATE TABLE words (id INTEGER PRIMARY KEY, name TEXT, meaning TEXT)',
+            'CREATE TABLE words(id INTEGER PRIMARY KEY, name TEXT, meaning TEXT)',
           );
         },
         version: 1,
       );
+
       return true;
     } catch (err) {
       print(err.toString());
+
       return false;
     }
   }
 
-  Future<bool> inserWord(Word word) async {
+  Future<bool> insertWord(Word word) async {
     final Database db = await database;
+
     try {
       db.insert(
         'words',
         word.toMap(),
         conflictAlgorithm: ConflictAlgorithm.replace,
       );
+
       return true;
     } catch (err) {
       return false;
     }
   }
 
-  Future<List<Word>> selectWords(int id) async {
+  Future<List<Word>> selectWords() async {
     final Database db = await database;
+
     final List<Map<String, dynamic>> data = await db.query('words');
 
-    return List.generate(
-      data.length,
-      (index) => Word(
-        id: data[index]['id'],
-        name: data[index]['name'],
-        meaning: data[index]['meaning'],
-      ),
-    );
+    return List.generate(data.length, (i) {
+      return Word(
+        id: data[i]['id'],
+        name: data[i]['name'],
+        meaning: data[i]['meaning'],
+      );
+    });
   }
 
   Future<Word> selectWord(int id) async {
     final Database db = await database;
+
     final List<Map<String, dynamic>> data =
         await db.query('words', where: "id = ?", whereArgs: [id]);
+
     return Word(
-      id: data[0]['id'],
-      name: data[0]['name'],
-      meaning: data[0]['meaning'],
-    );
+        id: data[0]['id'], name: data[0]['name'], meaning: data[0]['meaning']);
   }
 
   Future<bool> updateWord(Word word) async {
     final Database db = await database;
+
     try {
-      await db.update(
+      db.update(
         'words',
         word.toMap(),
         where: "id = ?",
         whereArgs: [word.id],
       );
+
       return true;
     } catch (err) {
       return false;
@@ -86,12 +92,14 @@ class DatabaseService {
 
   Future<bool> deleteWord(int id) async {
     final Database db = await database;
+
     try {
       db.delete(
-        'word',
+        'words',
         where: "id = ?",
         whereArgs: [id],
       );
+
       return true;
     } catch (err) {
       return false;
